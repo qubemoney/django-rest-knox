2e6b757106c9deefcf6eac3a29fd8f9fb95a33be
Mason Neipp <MasonGatesNeipp@gmail.com>
2023-12-22 09:41:27 -0700

    4.2.7 bump

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='4.2.5',
+    version='4.2.7',
     description='Custom Fork of django-rest-knox',
     long_description=long_description,
     long_description_content_type='text/markdown',
